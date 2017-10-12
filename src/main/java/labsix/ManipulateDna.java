e981259898cf1bcb68c6c820602a319f605597f1
Menachem Lattke <lattkem@aldenv155.allegheny.edu>
2017-10-12 14:53:57 -0400

    The assignment is done.

--- src/main/java/labsix/ManipulateDna.java
+++ src/main/java/labsix/ManipulateDna.java
@@ -3,13 +3,22 @@ package labsix;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Date;
+import java.util.Random;
 import java.util.Scanner;
+
+/** A javadoc comment for your reading pleasure.
+  * Info is the same as the assignment.
+  **/
 
 public class ManipulateDna {
 
+  /** Here's a concept:
+    * These are kind of unnecessary.
+    **/
+
   public static void main(String[] args) {
     // display the name of the programmer and the date
-    System.out.println("Gregory M. Kapfhammer " + new Date());
+    System.out.println("Till Lattke " + new Date());
     // declare the starting file and scanner
     File dnaInputsFile = null;
     Scanner scanner = null;
@@ -21,17 +30,37 @@ public class ManipulateDna {
       System.out.println("Unable to locate file");
     }
     // Step One: Read in and display the chosen DNA string
-
+    String dnainput = scanner.nextLine();
+    String dnaoriginal = dnainput.toUpperCase();
+    System.out.println("The DNA string worked with will be: " + dnaoriginal);
     // Step Two: Compute the complement of the DNA String
-
+    String dnac = dnainput.replace('a', 'T').replace('t', 'A').replace('g', 'C').replace('c', 'G');
+    System.out.println("The complementary string is: " + dnac);
     // Step Three: Insert a randomly chosen DNA letter into the DNA String
-
+    Random randomgen = new Random();
+    int randGene = randomgen.nextInt(4);
+    char addition = dnac.charAt(randGene);
+    String dnaadd = (addition + "" + dnac);
+    System.out.println("Inserting " + addition + " at position 0 gives "
+        + dnaadd); 
     // Step Four: Delete a DNA letter from a randomly chosen position in the DNA string
-
-    // Step Five: Change a random position in the DNA String to a randomly chosen DNA letter
-
+    int replacedef = randomgen.nextInt(4);
+    char removable = dnaadd.charAt(replacedef);
+    // Test: String dnasub = dnaadd.replace(removable, 'P');
+    String seg1 = dnaadd.substring(0, replacedef);
+    String seg2 = dnaadd.substring((replacedef + 1), 5);
+    String dnasub = (seg1 + "" + seg2);
+    System.out.println("Deleting position " + replacedef + " gives "
+        + dnasub);
+    // Change a rand position in the DNA String to a randomly chosen DNA letter
+    int randomgenereplacement = randomgen.nextInt(4);
+    int rdeff = randomgen.nextInt(4);
+    char randomreplacement = dnaoriginal.charAt(randomgenereplacement);
+    String dnamutie = dnasub.replace((dnasub.charAt(rdeff)), randomreplacement);
+    System.out.println("Replacing position " + rdeff + " with "
+        + randomgenereplacement + " gives " + dnamutie);
     // Step Six: Display a final thankyou message
-
+    System.out.println("Thanks for using ManipulateDNA. \nHave a day.");
   }
 
 }
--- writing/reflection.md
+++ writing/reflection.md
@@ -1,4 +1,14 @@
-# Reflection by
+# Reflection by Till
 
-This lab required me to write a file called `ManipulateDNA`. Please review the
-Laboratory Assignment Sheet and add the required details to this paragraph.
+This lab was simple and I have plans to replace my static generators with
+string lengths so that the program will be able to work with strings of any
+given length. The problem that I ran into was deleting a letter from the
+string. Where being able to use the replace method with the string to replace a
+character with nothing would have been ideal, I ended up deleting it through
+seperation into substrings. The idea behind this is that substrings are not
+inclusive of their end index, which made this an easy solution.
+
+Using string length in my next iteration of the program will be easily
+replacable, and result in the program likely containing no static int values. I
+could also use the scanner method with a prompt, which I find more fun to test
+a program with than from an input file.
